737a4956d7a9e1460c922c935120386f42ced78d
jaken92 <jaaakeeen@gmail.com>
2023-08-23 10:41:19 +0200

    changed some element names in nav

--- main.js
+++ main.js
@@ -1,56 +1,94 @@
 import './styles.css';
 
-// const width = window.innerWidth;
-// const height = window.innerHeight;
-
-// console.log('width:' + width + 'height:' + height);
-
 const scrollContainer = document.querySelector('.scroll-container');
-const sectionOne = document.querySelector('#sectionOne');
-const sectionTwo = document.querySelector('#sectionTwo');
-const sectionThree = document.querySelector('#sectionThree');
-const sectionFour = document.querySelector('#sectionFour');
-const sectionFive = document.querySelector('#sectionFive');
-const sectionSix = document.querySelector('#sectionSix');
-const sectionSeven = document.querySelector('#sectionSeven');
+const sectionOneAnchor = document.querySelector('#sectionOneAnchor');
+const sectionTwoAnchor = document.querySelector('#sectionTwoAnchor');
+const sectionThreeAnchor = document.querySelector('#sectionThreeAnchor');
+const sectionFourAnchor = document.querySelector('#sectionFourAnchor');
+const sectionFiveAnchor = document.querySelector('#sectionFiveAnchor');
+const sectionSixAnchor = document.querySelector('#sectionSixAnchor');
+const sectionSevenAnchor = document.querySelector('#sectionSevenAnchor');
+
+const navItems = [];
+
+//function pushing all the navanchors into array.
+function navElementsToArray(
+  element1,
+  element2,
+  element3,
+  element4,
+  element5,
+  element6,
+  element7
+) {
+  navItems.push(
+    element1,
+    element2,
+    element3,
+    element4,
+    element5,
+    element6,
+    element7
+  );
+}
+
+navElementsToArray(
+  sectionOneAnchor,
+  sectionTwoAnchor,
+  sectionThreeAnchor,
+  sectionFourAnchor,
+  sectionFiveAnchor,
+  sectionSixAnchor,
+  sectionSevenAnchor
+);
+
+//function reseting the scale of navitems to 1.0.
+function resetNavScaleing(navItems) {
+  navItems.forEach((element) => {
+    element.style.scale = 1.0;
+  });
+}
 
 const height =
   window.innerHeight ||
   document.documentElement.clientHeight ||
   document.body.clientHeight;
 
-// console.log(width);
-console.log(height);
-
 scrollContainer.addEventListener('scroll', function () {
   // const html = document.documentElement;
 
   const scrollY = scrollContainer.scrollTop;
-  console.log(scrollY);
 
   if (scrollY + height <= height * 1.5 || scrollY == 0) {
-    sectionOne.style.scale = 1.3;
+    resetNavScaleing(navItems);
+    sectionOneAnchor.style.scale = 1.3;
   }
 
   if (scrollY + height >= height * 1.5) {
-    sectionOne.style.scale = 1.0;
-    sectionTwo.style.scale = 1.3;
+    resetNavScaleing(navItems);
+    // sectionOneAnchor.style.scale = 1.0;
+    sectionTwoAnchor.style.scale = 1.3;
   }
   if (scrollY + height >= height * 2.5) {
-    sectionTwo.style.scale = 1.0;
-    sectionThree.style.scale = 1.3;
+    resetNavScaleing(navItems);
+    // sectionTwoAnchor.style.scale = 1.0;
+    sectionThreeAnchor.style.scale = 1.3;
   }
   if (scrollY + height >= height * 3.5) {
-    sectionThree.style.scale = 1.0;
-    sectionFour.style.scale = 1.3;
+    resetNavScaleing(navItems);
+    // sectionThreeAnchor.style.scale = 1.0;
+    sectionFourAnchor.style.scale = 1.3;
   }
   if (scrollY + height >= height * 4.5) {
-    sectionFive.style.scale = 1.3;
+    resetNavScaleing(navItems);
+    sectionFiveAnchor.style.scale = 1.3;
   }
   if (scrollY + height >= height * 5.5) {
-    sectionSix.style.scale = 1.3;
+    resetNavScaleing(navItems);
+    sectionSixAnchor.style.scale = 1.3;
   }
   if (scrollY + height >= height * 6.5) {
-    sectionSeven.style.scale = 1.3;
+    resetNavScaleing(navItems);
+    sectionSevenAnchor.style.scale = 1.3;
   }
 });
