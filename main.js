4d670baa0ef86911d834935c620b9032a3ee5fea
jaken92 <jaaakeeen@gmail.com>
2023-08-23 09:56:54 +0200

    js + css

--- main.js
+++ main.js
@@ -5,32 +5,52 @@ import './styles.css';
 
 // console.log('width:' + width + 'height:' + height);
 
-const about = document.querySelector('#abt');
-const width =
-  window.innerWidth ||
-  document.documentElement.clientWidth ||
-  document.body.clientWidth;
+const scrollContainer = document.querySelector('.scroll-container');
+const sectionOne = document.querySelector('#sectionOne');
+const sectionTwo = document.querySelector('#sectionTwo');
+const sectionThree = document.querySelector('#sectionThree');
+const sectionFour = document.querySelector('#sectionFour');
+const sectionFive = document.querySelector('#sectionFive');
+const sectionSix = document.querySelector('#sectionSix');
+const sectionSeven = document.querySelector('#sectionSeven');
+
 const height =
   window.innerHeight ||
   document.documentElement.clientHeight ||
   document.body.clientHeight;
 
-console.log(width);
+// console.log(width);
 console.log(height);
 
-window.addEventListener('scroll', function () {
-  const html = document.documentElement;
-  //getting the document height, reference: https://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript
-  //   const documentHeight = Math.max(
-  //     body.scrollHeight,
-  //     body.offsetHeight,
-  //     html.clientHeight,
-  //     html.scrollHeight,
-  //     html.offsetHeight
-  //   );
-  const scrollY = window.scrollY;
+scrollContainer.addEventListener('scroll', function () {
+  // const html = document.documentElement;
+
+  const scrollY = scrollContainer.scrollTop;
   console.log(scrollY);
-  if (scrollY + height >= height + 0.5 * height) {
-    about.style.scale = 2.5;
+
+  if (scrollY + height <= height * 1.5 || scrollY == 0) {
+    sectionOne.style.scale = 1.3;
+  }
+
+  if (scrollY + height >= height * 1.5) {
+    sectionOne.style.scale = 1.0;
+    sectionTwo.style.scale = 1.3;
+  }
+  if (scrollY + height >= height * 2.5) {
+    sectionTwo.style.scale = 1.0;
+    sectionThree.style.scale = 1.3;
+  }
+  if (scrollY + height >= height * 3.5) {
+    sectionThree.style.scale = 1.0;
+    sectionFour.style.scale = 1.3;
+  }
+  if (scrollY + height >= height * 4.5) {
+    sectionFive.style.scale = 1.3;
+  }
+  if (scrollY + height >= height * 5.5) {
+    sectionSix.style.scale = 1.3;
+  }
+  if (scrollY + height >= height * 6.5) {
+    sectionSeven.style.scale = 1.3;
   }
 });
