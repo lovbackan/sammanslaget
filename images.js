35138a639ce8dcdb6aeafc57587724bc8e4d8a0d
jaken92 <jaaakeeen@gmail.com>
2023-08-24 13:24:31 +0200

    js for fullscreen img added

---
+++ images.js
@@ -0,0 +1,33 @@
+// const images = document.querySelector('.pop-img');
+// // const popimg = document.querySelector('#pop');
+
+// images.addEventListener('click', () => {
+//   console.log('click');
+// });
+
+// const backgroundImage = 'url(' + images.src + ')';
+// console.log(backgroundImage);
+
+
+
+//https://www.w3schools.com/jsref/met_element_requestfullscreen.asp
+/* Get the element you want displayed in fullscreen mode (a video in this example): */
+var elem = document.querySelector('.pop-img');
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen property yet */
+elem.addEventListener('click', () => {
+  openFullscreen(elem);
+});
+
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
