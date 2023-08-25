9aac2dca562dd535d3e38e68bbdfc32e114acd48
jaken92 <jaaakeeen@gmail.com>
2023-08-25 11:24:19 +0200

    Trash

--- images.js
+++ images.js
@@ -1,8 +1,7 @@
 //https://www.w3schools.com/jsref/met_element_requestfullscreen.asp
-/* Get the element you want displayed in fullscreen mode (a video in this example): */
 
-// /* When the openFullscreen() function is executed, open the video in fullscreen.
-// Note that we must include prefixes for different browsers, as they don't support the requestFullscreen property yet */
+//Images to fullscreen on click.
+
 var elem = document.querySelectorAll(".pop-img");
 
 elem.forEach((item) => {
@@ -22,10 +21,3 @@ function openFullscreen(elem) {
     elem.msRequestFullscreen();
   }
 }
-
-// elem.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     console.log("hejehej");
-//     item.classList.add("focus");
-//   });
-// });
